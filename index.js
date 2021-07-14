f7578407ccb49a6bda37af63725c7d891bb792cd
Sergio Santiago <sergiosantiago0403@gmail.com>
2021-07-14 18:21:25 +0200

    commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 const bodyParse = require("body-parse");
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT;
 
 //mysql://b997872b53beea:2a43ebc9@us-cdbr-east-04.cleardb.com/heroku_8161b00dcfaf10f
 
