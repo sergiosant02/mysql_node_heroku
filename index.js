ee29b852d24d646e7794e4418c683609dcbf0af8
Sergio Santiago <sergiosantiago0403@gmail.com>
2021-07-14 18:09:40 +0200

    config mysql

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql");
 
+//mysql://b997872b53beea:2a43ebc9@us-cdbr-east-04.cleardb.com/heroku_8161b00dcfaf10f
+
 const conexion = mysql.createConnection({
-    host:"",
-    database:"",
-    user:"",
-    password:""
+    host:"us-cdbr-east-04.cleardb.com",
+    database:"heroku_8161b00dcfaf10f",
+    user:"b997872b53beea",
+    password:"2a43ebc9"
 });
 
 conexion.connect(function(error){
