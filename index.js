f0abe42cb1d2733727d7a536217cb40ba262b736
Sergio Santiago <sergiosantiago0403@gmail.com>
2021-07-15 10:11:15 +0200

    g

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/hola', (req,res)=>{
 });
 
 app.get('/', (req, res) => {
-    const sql = 'SELECT * FROM prueba';
+    const sql = 'SELECT * FROM prueba WHERE id = 15';
     
 
     conexion.query(sql, (err, result,fields)=>{
