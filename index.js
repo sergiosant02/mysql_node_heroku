7727e8606dc97dc30126ecdc9d877100240c6be3
Sergio Santiago <sergiosantiago0403@gmail.com>
2021-07-15 14:05:58 +0200

    poool

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bp.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 8080));
 //mysql://b997872b53beea:2a43ebc9@us-cdbr-east-04.cleardb.com/heroku_8161b00dcfaf10f
 
-const conexion = mysql.createConnection({
+const pool = mysql.createPool({
     //timeout:3000,
     host:"us-cdbr-east-04.cleardb.com",
     database:"heroku_8161b00dcfaf10f",
@@ -21,54 +21,58 @@ const conexion = mysql.createConnection({
 });
 
 conexion.connect();
+pool.getConnection((err, conexion) =>{
+    if(err) throw err;
+    app.get('/h', (req,res)=>{
+        res.send("Welcome");
+    });
+    
+    app.get('/hola', (req,res)=>{
+        res.send("holas");
+    });
+    
+    app.get('/', (req, res) => {
+        const sql = 'SELECT * FROM productos';
+        
+    
+        conexion.query(sql, (err, result,fields)=>{
+            if (err){
+                console.log('errores: ' + err);
+                throw err;
+            }
+            //res.send(result)
+            res.json(result);
+            
+        });
+        
+    });
+    
+    app.post('/p', (request, response) => {
+        
+        const post = {nombre: "hola2", precio: 2.50, tipo: "kl", descripcion: "vrvvg", foto:"frvrvr"};
+        const data = request.body;
+        const p = new Productos(data.nombre, data.precio, data.tipo, data.descripcion, data.foto);
+        console.log("Cuerpo del post:")
+        console.log("Cuerpo del post: "+data);
+        conexion.query('INSERT INTO productos SET ?', p, (err, result) => {
+            if (err) {
+                console.log("Cuerpo del post:")
+                console.log("Cuerpo del post: "+p);
+                console.log('error: ' + err);
+                //throw err;
+            } else {
+    
+                response.send("ok");
+            }
+            //response.status(201).send(`Producto added with ID: ${result.insertId}`);
+        });
+    });
+});
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
-app.get('/h', (req,res)=>{
-    res.send("Welcome");
-});
 
-app.get('/hola', (req,res)=>{
-    res.send("holas");
-});
-
-app.get('/', (req, res) => {
-    const sql = 'SELECT * FROM productos';
-    
-
-    conexion.query(sql, (err, result,fields)=>{
-        if (err){
-            console.log('errores: ' + err);
-            throw err;
-        }
-        //res.send(result)
-        res.json(result);
-        
-    });
-    
-});
-
-app.post('/p', (request, response) => {
-    
-    const post = {nombre: "hola2", precio: 2.50, tipo: "kl", descripcion: "vrvvg", foto:"frvrvr"};
-    const data = request.body;
-    const p = new Productos(data.nombre, data.precio, data.tipo, data.descripcion, data.foto);
-    console.log("Cuerpo del post:")
-    console.log("Cuerpo del post: "+data);
-    conexion.query('INSERT INTO productos SET ?', p, (err, result) => {
-        if (err) {
-            console.log("Cuerpo del post:")
-            console.log("Cuerpo del post: "+p);
-            console.log('error: ' + err);
-            //throw err;
-        } else {
-
-            response.send("ok");
-        }
-        //response.status(201).send(`Producto added with ID: ${result.insertId}`);
-    });
-});
 
 
 
