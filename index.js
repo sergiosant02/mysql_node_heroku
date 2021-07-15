2a6df396a7ed08d9bfa66fca537b11dc89121c84
Sergio Santiago <sergiosantiago0403@gmail.com>
2021-07-15 10:07:41 +0200

    j

--- index.js
+++ index.js
@@ -8,42 +8,41 @@ app.set('port', (process.env.PORT || 8080))
 //mysql://b997872b53beea:2a43ebc9@us-cdbr-east-04.cleardb.com/heroku_8161b00dcfaf10f
 
 const conexion = mysql.createConnection({
-    timeout:3000,
+    //timeout:3000,
     host:"us-cdbr-east-04.cleardb.com",
     database:"heroku_8161b00dcfaf10f",
     user:"b997872b53beea",
     password:"2a43ebc9"
 });
 
-
+conexion.connect();
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-
-    app.get('/h', (req,res)=>{
-        res.send("Welcomes");
-    });
-
-    app.get('/hola', (req,res)=>{
-        res.send("holas");
-    });
-    
-    app.get('/prueba', (req, res) => {
-        const sql = 'SELECT * FROM prueba';
-        conexion.connect();
-    
-        conexion.query(sql, (err, result,fields)=>{
-            if (err){
-                console.log('error');
-                throw err;
-            }
-            
-                res.json(result);
-            
-        });
-        conexion.end();
-    });
   });
 
+app.get('/h', (req,res)=>{
+    res.send("Welcome");
+});
+
+app.get('/hola', (req,res)=>{
+    res.send("holas");
+});
+
+app.get('/', (req, res) => {
+    const sql = 'SELECT * FROM prueba';
+    
+
+    conexion.query(sql, (err, result,fields)=>{
+        if (err){
+            console.log('error: ' + err);
+            throw err;
+        }
+        res.send(result)
+            //res.json(result);
+        
+    });
+    
+});
 
 
 
